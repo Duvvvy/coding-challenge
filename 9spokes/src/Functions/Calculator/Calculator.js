bd9dc95636a6933bdd5fe04f66202a04fc197691
Duvvvy <ryanbircham@gmail.com>
2021-03-03 15:31:37 +1300

    Refactor name company > record

--- 9spokes/src/Functions/Calculator/Calculator.js
+++ 9spokes/src/Functions/Calculator/Calculator.js
@@ -3,11 +3,11 @@ export function filterByListOfCategories(data, categoryList){// Only returns rec
 }
 
 export function filterByListOfAccountTypes(data, listOfAccountTypes){//Only return companies of given type/s in list.
-    return data.filter(company => listOfAccountTypes.includes(company.account_type));
+    return data.filter(record => listOfAccountTypes.includes(record.account_type));
 }
 
 export function filterByListOfValueType(data, listOfValueTypes){
-    return data.filter(company => listOfValueTypes.includes(company.value_type))
+    return data.filter(record => listOfValueTypes.includes(record.value_type))
 }
 
 export function sumReduce(data){
