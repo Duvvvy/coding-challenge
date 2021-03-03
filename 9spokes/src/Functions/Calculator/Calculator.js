e419415c86ece2f6aac12468f6b4aa24214f3445
Duvvvy <ryanbircham@gmail.com>
2021-03-03 15:14:40 +1300

    Outline some tests

---
+++ 9spokes/src/Functions/Calculator/Calculator.js
@@ -0,0 +1,2 @@
+export function filterByListOfCategories(data, categoryList){
+}
---
+++ 9spokes/src/Functions/Calculator/Calculator.test.js
@@ -0,0 +1,17 @@
+import {
+    filterByListOfCategories, 
+  } from './Calculator';
+  
+  test('return only records that are in list of categories function', () => {
+
+  });
+  
+  test('return only records that are in list of account types function', () => {
+
+  });
+  
+  test('return only records that are in list of value types function', () => {
+
+  });
+
+  
